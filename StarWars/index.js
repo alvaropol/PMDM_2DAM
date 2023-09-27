d54aa8996d029aa9e821f065d2dc861164495012
alvaropol <alvaro.pold@gmail.com>
2023-09-27 14:30:38 +0200

    acabar det

--- StarWars/index.js
+++ StarWars/index.js
@@ -5,16 +5,22 @@ $(document).ready(function () {
     }).done(function (resp) {
         var listadoPersonajes = resp.results;
         listadoPersonajes.forEach(function (personaje) {
-            // var template = '<p><h1>' + pokemon.name + '</h1></p>';;
             var template =
-                `<h5 class="card-title text-warning">${personaje.name}</h5>
-            <hr style="color: white;">
-            <p class="card-text text-white">
-            ${personaje.birth_year}
+                `<div class="col">
+                <a href="#modal" title="Foto de Darth Maul" class="btn"><img
+                src="https://starwars-visualguide.com/assets/img/characters/44.jpg" class="card-img-top"
+                alt="Skyscrapers" /></a>
+                <div class="card-body bg-dark">
+                <h5 class="card-title text-warning">${personaje.name}</h5>
+                <hr style="color: white;">
+                <p class="card-text text-white">
+                ${personaje.birth_year}
                 <br>
                 ${personaje.gender}
-            </p>`
-            $('#tarjeta').append(template);
+            </p>
+            </div>
+            </div>`
+            $('#row1').append(template);
         });
     });
 
