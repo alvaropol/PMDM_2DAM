a9d14b3c5b61979dfb6049569b6e8266efd6e7bd
Álvaro Polonio Domínguez <alvaro.pold@gmail.com>
2023-10-12 19:45:54 +0200

    Ej 01, 02 add

--- ejemploAngular/src/app/app.module.ts
+++ ejemploAngular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ComponentCarComponent } from './component-car/component-car.component';
+import { RestaurantCardComponentComponent } from './restaurant-card-component/restaurant-card-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentCarComponent,
+    RestaurantCardComponentComponent,
   ],
   imports: [
     BrowserModule,
---
+++ ejemploAngular/src/app/restaurant-card-component/restaurant-card-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-card-component',
+  templateUrl: './restaurant-card-component.component.html',
+  styleUrls: ['./restaurant-card-component.component.css']
+})
+export class RestaurantCardComponentComponent {
+
+}
