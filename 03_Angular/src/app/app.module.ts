c907cae48b39b23eedca70d96c7283887a55cbc0
alvaropol <alvaro.pold@gmail.com>
2023-10-16 13:32:44 +0200

    calculator

--- 03_Angular/src/app/app.module.ts
+++ 03_Angular/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './ui/toolbar/toolbar.component';
+import { HomeScreenComponent } from './ui/home-screen/home-screen.component';
+import { TextComponent } from './ui/text/text.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    HomeScreenComponent,
+    TextComponent
   ],
   imports: [
     BrowserModule
--- ejemploAngular/src/app/app.module.ts
+++ ejemploAngular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ComponentCarComponent } from './component-car/component-car.component';
 import { RestaurantCardComponentComponent } from './restaurant-card-component/restaurant-card-component.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentCarComponent,
     RestaurantCardComponentComponent,
+    CalculadoraComponent,
   ],
   imports: [
     BrowserModule,
