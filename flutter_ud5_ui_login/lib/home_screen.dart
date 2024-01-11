9ccd20f5f6ffeaf537c7fa38498691378a63cf57
alvaropol <alvaro.pold@gmail.com>
2024-01-11 08:36:58 +0100

    exercise finished

--- flutter_ud5_ui_login/lib/home_screen.dart
+++ flutter_ud5_ui_login/lib/home_screen.dart
@@ -18,7 +18,86 @@ class MyLogin extends StatelessWidget {
                   width: 230,
                 ),
               ),
-              const Text('Beautiful, Private Sharing')
+              const SizedBox(height: 6),
+              const Text(
+                'Beautiful, Private Sharing',
+                style: TextStyle(color: Colors.grey, fontSize: 20),
+              ),
+              const SizedBox(height: 160),
+              const FilledButton(
+                  onPressed: null,
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.white),
+                    shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5)))),
+                    fixedSize: MaterialStatePropertyAll(
+                      Size(250, 55),
+                    ),
+                  ),
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 224, 26, 26),
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                  )),
+              const SizedBox(height: 30),
+              const Text(
+                'Already have a Path account?',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              const SizedBox(height: 10),
+              const FilledButton(
+                  onPressed: null,
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll(Colors.transparent),
+                    shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                        side: BorderSide(color: Colors.white54))),
+                    fixedSize: MaterialStatePropertyAll(
+                      Size(250, 55),
+                    ),
+                  ),
+                  child: Text(
+                    'Log In',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 22,
+                        fontWeight: FontWeight.normal),
+                  )),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: 250,
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: const TextSpan(
+                    text: 'By using Path, you agree to Paths ',
+                    style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.grey,
+                        fontSize: 16),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'Terms of Use ',
+                          style: TextStyle(
+                              color: Colors.white,
+                              decoration: TextDecoration.underline)),
+                      TextSpan(
+                          text: 'and ',
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              color: Colors.grey,
+                              fontSize: 16)),
+                      TextSpan(
+                          text: 'Privacy Policy',
+                          style: TextStyle(
+                              color: Colors.white,
+                              decoration: TextDecoration.underline))
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ));
